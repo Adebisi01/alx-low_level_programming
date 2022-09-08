6f666b7b4c1894309797b6d305f39fde8e63e095
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-08 16:35:15 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - function prints exact text
+ * Return: 0 by default
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
