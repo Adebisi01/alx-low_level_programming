839cc5f24a9a2ab066809d5e10d06a92a9c66a4d
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-08 16:15:03 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ *The function prints out corresponding string
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
