5e058c352b95f2367e298043867339e14f84ae06
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-09 15:21:30 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets
+ * Return: 0 by default
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
