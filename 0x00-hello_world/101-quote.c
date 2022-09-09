3afc5b5407266abfb1f691e75791b1b7897854cb
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-08 17:35:53 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - prints stderr
+ * Return: 1 by default
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
