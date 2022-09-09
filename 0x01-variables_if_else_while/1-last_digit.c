9600b20a951830c2bfd7dd69b63a29b3b74c97d3
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-09 14:21:02 -0700

    last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,7 +13,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	last = n % 10;
-	if(last > 5)
+	if (last > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5", n, last);
 	} else if (last == 0)
@@ -23,4 +23,5 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, last);
 	}
-}	
+	return (0);
+}
