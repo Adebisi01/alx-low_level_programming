4546cc95ad13348bc9600700f10adbd1caadc3e9
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-09 14:56:09 -0700

    lower upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets
+ * Return: 0 by default
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	for(i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
