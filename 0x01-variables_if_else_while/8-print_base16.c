fbab24bf78a7dc5fb4d6ca93b8be277690d76d35
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-09 15:38:49 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets
+ * Return: 0 by default
+ */
+int main(void)
+{
+	char i;
+
+	for (j = 0; j < 10; j++)
+	{
+		putchar(i + '0')
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
