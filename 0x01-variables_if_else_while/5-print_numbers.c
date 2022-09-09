982e21c271cc9e721b0fae9ece3a9b986490a7e1
Adebisi OLuwafemi <oluwafemiadebisi01@gmail.com>
2022-09-09 15:03:23 -0700

    digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints digits
+ * Return: 0 by default
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
