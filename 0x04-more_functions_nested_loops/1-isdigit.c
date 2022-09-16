e602b6bad95cba5b1829deb4b540bb9f1c582f88
Adebisi <oluwafemiadebisi01@gmail.com>
2022-09-15 22:24:38 -0700

    digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checkes digit
+ *
+ * @c: character to check
+ * Return - 1 or 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && <= 9)
+	{
+		return (1);
+	}
+		return (0);
+}
+
