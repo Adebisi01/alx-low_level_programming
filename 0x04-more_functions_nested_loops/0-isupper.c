21dc25a9a370aa934e4cde7312a62326b08ea3f1
Adebisi <oluwafemiadebisi01@gmail.com>
2022-09-15 22:16:35 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,15 @@
 /**
  * _isupper  - check the code.
  *
+ * @c: char to check
+ *
  * Return: Always 0
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		return (1)
+		return (1);
 	}
 	else
 	{
