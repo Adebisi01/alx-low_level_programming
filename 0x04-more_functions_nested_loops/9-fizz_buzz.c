f51aa14160135b76c08a5bc1b577fcdab69c7d5a
Adebisi <oluwafemiadebisi01@gmail.com>
2022-09-16 18:26:19 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,11 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if(i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -23,6 +27,7 @@ int main(void)
 			printf("%d ", i);
 		}
 	}
+	printf("\n");
 	return (0);
 }
 
