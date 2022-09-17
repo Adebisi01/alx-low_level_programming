fc5799afbb36ee7e169b23113d3ac6af8a01bf0a
Adebisi <oluwafemiadebisi01@gmail.com>
2022-09-16 17:57:42 -0700

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print square with hash
+ * @size: size of square
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
