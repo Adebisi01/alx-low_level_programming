d6e5546fe505885bbf6f709906f04e8e7fafb199
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-19 22:21:00 +0100

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,17 @@
 void rev_string(char *s)
 {
 	int len;
-	
+	int i;
+	char str[10];
+
 	len = 0;
+	i = 0;
 	while (*(s + len) != '\0')
 	{
 		len++;
 	}
 	for (--len; len >= 0; len--)
 	{
-		_putchar(s[len]);
+		str[i] = s[len];		
 	}
-}
+	*s = str;}
