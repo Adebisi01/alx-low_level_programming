1e4f361a253133d6a5ed5f43e192cefce4dc90f6
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-19 22:10:59 +0100

    merge

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,21 @@
-#include
+#include "main.h"
+
+/**
+ * rev_string - rverses strings
+ * @s: string
+ * Return: nothing
+ */
+void rev_string(char *s)
+{
+	int len;
+	
+	len = 0;
+	while (*(s +len) != '\0')
+	{
+		len++;
+	}
+	for (--len; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+}
