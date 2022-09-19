6dcf6b18393d0051d20fe9c52a131d358625fbf7
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-19 23:42:48 +0100

    print n

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,14 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while(i < n)
+	while (i < n)
 	{
 		printf("%d, ", *(a + i));
+		if (n != (i + 1))
+		{
+			printf("%d, ", *(a + i));
+		} else
+			printf("%d", *(a + i));
 		i++;
 	}
 }
