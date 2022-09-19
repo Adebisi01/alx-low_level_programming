4c38a9c0caf1f5b1402a440a2341fd7041b2e8e7
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-19 21:04:01 +0100

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,19 @@
-#
+#include "main.h"
+
+/**
+ * print_rev - print characters in reverse
+ * @s: character
+ * @size: character size
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int size;
+
+	size = sizeof(s) - 1;
+	while(size >= 0)
+	{
+		puts(s[size]);
+		size--;
+	}
+}
