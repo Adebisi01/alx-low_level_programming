5efeb88daf1ed9e594078f0bb6bdbbc43d59c146
oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-19 13:58:35 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,14 @@
 void print_rev(char *s)
 {
 	int len = 0;
+
 	while (*(s + len) != '\0')
 	{
 		len++;
 	}
-	for (; len > 0; len--)
+	for (--len; len >= 0; len--)
 	{
 		_putchar((s[len]));
 	}
-	_putchar('\n');	
+	_putchar('\n');
 }
