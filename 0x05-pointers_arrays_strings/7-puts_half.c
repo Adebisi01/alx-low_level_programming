01ff664771e53317a5d65f93c326125231bb0f9c
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-19 22:53:26 +0100

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half(char *str)
+ * @str: string
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int j;
+
+	while (*(str + i ) == '\0')
+	{
+		i++;
+	}
+	for (j = 0; j <= i/2; j++)
+	{
+		_putchar(str[j]);
+	}
+}	
