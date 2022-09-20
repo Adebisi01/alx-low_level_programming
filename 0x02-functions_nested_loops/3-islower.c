27eed007cdb1538720bf42bdcd63021243b3eae1
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 16:33:39 +0100

    check lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - checks lower characters
+ * @c: character
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	int i = 0;
+
+	for (i = 97; i <= 122; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+			return (0);
+
+	}
+}
