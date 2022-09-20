6049ca28e8d09cc4dc558fdae98cceb3d1cefb02
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 17:43:56 +0100

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit
+ * @n: integer
+ * Return: last digit
+ */
+int print_last_digit(int n);
+{
+	int i = 0;
+
+	if (n <= 9)
+	{
+		i = n;
+	}
+	else if (n > 9)
+	{
+		i = n % 10;
+	}
+	return (i);
+}
