66887bdfbaccb4b89f4e2ac7e7a4e7e6edf68ee2
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 16:23:06 +0100

    putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabets 10time
+ * Return: nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	int j = 0;
+
+	while (i <= 9)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
