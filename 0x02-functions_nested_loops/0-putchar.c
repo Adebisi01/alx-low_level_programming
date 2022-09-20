e3b7c33c957cf39e076cfe00b07a78c020e5d67d
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 16:05:34 +0100

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,7 +4,13 @@
  * Return - 0
  */
 int main(void)
-{
-	char word = "_putchar";
-	_putchar(word);
+{	
+	_putchar(95);
+	_putchar(112);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	return (0);
 }
