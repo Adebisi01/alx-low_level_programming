e06a2742bf364b1fb654ab4a88330205538b6b01
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 17:34:39 +0100

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - returns absolute value
+ * @n: integer
+ * Return: absolute;
+ */
+int _abs(int n)
+{
+	int i = 0;
+
+	if (n < 0)
+	{
+		i = (n * -1)
+	}
+	else if (n > 0)
+	{
+		i = n;
+	}
+	return (i);
+}
