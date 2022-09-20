a3b660f5012e773f01a18ff881fb383c597dd21f
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 21:24:41 +0100

    to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,18 +7,18 @@
 void print_to_98(int n)
 {
 	int i = 0;
-	
+
 	printf("%d", n);
-	if(n < 98)
+	if (n < 98)
 	{
 		for (i = (n + 1); i <= 98; i++)
 		{
 			printf(", %d", i);
 		}
 	}
-	else 
+	else
 	{
-		for(i = (n - 1); i >= 98; i--)
+		for (i = (n - 1); i >= 98; i--)
 		{
 			printf(", %d", i);
 		}
