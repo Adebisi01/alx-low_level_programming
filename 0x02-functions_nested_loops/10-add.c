f0a687ca592639e68d20e36b8fef6b71b99cfd37
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 19:58:25 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @n: first int
+ * @m: second int
+ * Return: interger
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ * Return: nothing
+ */
+void times_table(void)
+{
+	int i = 0;
+
+	while (
