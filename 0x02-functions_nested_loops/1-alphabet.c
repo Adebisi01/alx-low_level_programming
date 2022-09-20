0977a1aa62bbc251bea092e176cf792d3ac3ad72
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-20 16:14:21 +0100

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints all alphabets
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	int i = 0;
+
+	for (i = 97; i <= 112; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
