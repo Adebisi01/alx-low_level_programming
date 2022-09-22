a7aee8786ed18d94440d95d1f45710fbbd8a1cba
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-22 20:53:21 +0100

    n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,15 +4,22 @@
  * *_strncat - concatenate two strings
  * @dest: destination
  * @src: source
+ * @n: limit
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0;
-	int len = strlen(dest);
+	int len = 0;
+
+	while (*(dest + len) == '\0')
+	{
+		len++;
+	}
 
 	for (i = 0; i <= n; i++)
 	{
-		dest[len + i] = src[i];
+		*(dest + len + i) = *(src + i);
 	}
 	return (dest);
 }
