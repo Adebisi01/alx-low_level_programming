7f73e1d83122c3a73cadd7481cb78b4b7415fbc1
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-22 21:34:43 +0100

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,15 +10,15 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	if (s1 > s2)
+	if (*s1 > *s2)
 	{
-		i = (s1 - s2);
+		i = (*s1 - *s2);
 	}
-	else if (s2 > s1)
+	else if (*s2 > *s1)
 	{
-		i = (s1 - s2);
+		i = (*s1 - *s2);
 	}
-	else if (s1 == s2)
+	else if (*s1 == *s2)
 	{
 		i = 0;
 	}
