36b9023f1fb0a0d8b4fa835339d351e3ab8e3729
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-23 04:30:05 +0100

    encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,17 +2,17 @@
 
 /**
  * leet - encodes to 1337
- * s - string
+ * @s: string
  * Return: encoded string
  */
 char *leet(char *s)
 {
-	char cap = ['A', 'E', 'O', 'T', 'L'];
-	char small = ['a', 'e', 'o', 't', 'l'];
-	int rep = [4, 3, 0, 7, 1];
+	char cap[] = {'A', 'E', 'O', 'T', 'L'};
+	char small[] = {'a', 'e', 'o', 't', 'l'};
+	int rep[] = {4, 3, 0, 7, 1};
 	int i;
 	int l;
-	
+
 	for (i = 0; *(s + 1) != '\0'; i++)
 	{
 		for (j = 0; j < 5; j++)
