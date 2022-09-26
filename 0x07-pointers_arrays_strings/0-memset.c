4b5282c3f1a98db376fbab80129a4e1cdf277a5a
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-26 18:55:00 +0100

    constant buffer

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - fills a memory with buffer
+ * @s: buffer to fill
+ * @b: constant byte
+ * @n: number of bytes to fill
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	char **p;
+
+	for (i = 0; i <= n; i++)
+	{
+		*(s + i) = b;
+	}
+	**p = &s;
+	return (*p);
+}
