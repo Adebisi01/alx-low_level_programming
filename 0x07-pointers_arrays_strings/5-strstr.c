9bc9d867cf69606750c969d5046e52448947ebe8
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-27 04:54:12 +0100

    substr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - find needle
+ * @haystack: string
+ * @needle: substring
+ * Return: pointer
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int f = 0;
+	int ndllen = 0;
+
+	for (ndllen = 0; *(needle + ndllen) != 0 ; ndllen++)
+	{
+		;
+	}
+
+	for (i = 0; *(haystack + i) == *(needle + i); i++)
+	{
+		f = 1;
+		if (*(needle + i) == '\0' && f == 1)
+		{
+			return (haystack + i - ndllen);
+		}
+	}
+	return ('\0');
+}
