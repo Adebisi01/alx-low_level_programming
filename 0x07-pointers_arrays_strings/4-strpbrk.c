09c085f939ccd239fbb679d1335c79bb4a02fabd
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-27 04:16:11 +0100

    first

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - returns first occurence of byte
+ * @s: string
+ * @accept: char
+ * Return: pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int j;
+	char *ptr;
+
+	for (i = 0; *(s + i) != '\0'; si+)
+	{
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				ptr = &(*(s + i));
+			}
+			break;
+		}
+	}
+	return (ptr);
+}
