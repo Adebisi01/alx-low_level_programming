c353b08834e7156cee73bd0040a690c19beb3816
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-27 05:47:01 +0100

    strchar

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,14 @@ char *_strchr(char *s, char c)
 {
 	int i;
 	int strlen;
-	char *ptr;
 
 	for (strlen = 1; *(s + strlen) != '\0'; strlen++)
 		;
 	for (i = 0; i < strlen; i++)
 	{
 		if (*(s + i) == c)
-			ptr = &(*(s + i));
-		else
-			ptr = NULL;
+			return (s + i);
 	}
-	return (ptr);
+	return ('\0');
 }
 
