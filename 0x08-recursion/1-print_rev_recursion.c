303fd5dd88b931bcaf80c0f749dbf807c3d1a857
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-28 21:06:50 +0100

    factorial

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,14 +8,13 @@
 void _print_rev_recursion(char *s)
 {
 	int i = 0;
-	int j = 0;
+	int close = 0;
 	
-	if (*(s + i) != '\0' && j == 0)
+	if (*s == '\0' && close = 0)
 	{
-		i++;
-		_print_rev_recursion(s + i);
+		i = (1 + _print_rev_recursion(s + 1));
 	}
-	j = 1;
+	close = 1;
 	if (*(s + i) == '\n')
 	{
 		_putchar('\n');
