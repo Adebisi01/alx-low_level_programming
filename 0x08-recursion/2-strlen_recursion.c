8b37abaeb36d474c56bb7768a5a4cde1c4cd2ecc
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-28 20:30:51 +0100

    length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,3 +1,17 @@
 #include "main.h"
 
 /**
+ * _strlen_recursion - get string length
+ * @s: string
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*(s + i) == '\0')
+		return;
+	i++;
+	_strlen_recursion(s + i);
+	return (i);
+}
