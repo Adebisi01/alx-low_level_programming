a69bd50a6d7b4a5bcf909a1533a69126a5492e3f
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-28 21:33:25 +0100

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * is_prime_number - checks prime number
+ * @n: number
+ * Return: 1 or 0;
+ */
+int is_prime_number(int n)
+{
+	if (n % n = 0)
+		return (1);
+	return (0);
+}
