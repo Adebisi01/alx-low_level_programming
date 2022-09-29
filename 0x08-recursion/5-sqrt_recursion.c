d8b21412dc1e9d69a7ecf2de6e25764afb0c78ec
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-29 03:54:57 +0100

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,12 +5,22 @@
  * @n: integer
  * Return: square root
  */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return(-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
 int _sqrt_recursion(int n)
 {
 	int i = 1;
-	if (n / i == 1 && n % i == 0)
-		return (i);
-	i++;
-	return (_sqrt_recursion(n));
-
+	if (n < 0 )
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (_pow_recursion(n, 2));
 }
