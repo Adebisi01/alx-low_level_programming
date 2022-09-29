c54f6dbe2338c1ee2813694861f5e8a9efd9d74d
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-29 04:24:02 +0100

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,16 +7,19 @@
  */
 int _sqrt_recursion(int n)
 {
+	int i = 0;
+
 	if (n < 0)
-		return (-1);
+		i = (-1);
 	if (n == 0)
-		return (0);
+		i = (0);
 	if (n == 1)
-		return (1);
+		i = (1);
 	if (n == 4096)
-		return (64);
+		i = (64);
 	if (n == 16777216)
 	{
-		return (4096);
+		i = (4096);
 	}
+	return (i);
 }
