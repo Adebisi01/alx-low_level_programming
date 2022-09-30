28001995eb1a7f1d8feb5d0b3ccf0a9533acfad7
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-30 23:52:01 +0100

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,10 +9,19 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc = 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
+	}
+	if (!isdigit(atoi(argv[1])) || !isdigit(atoi(argv[2])))
+	{
+		printf("Error\n");
+		return (1)
+	}
+	if (!isdigit(atoi(argv[1])) && !isdigit(atoi(argv[2])))
+	{
+		printf("%d\n", 0);
 	}
 	if (argv <= 1)
 	{
