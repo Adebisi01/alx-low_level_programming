9666a999ae8c132c4b2a5cb7e4260638ed4c372c
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-30 22:21:07 +0100

    arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints its own name
+ * @argv: argv
+ * @argc: argc
+ * Return: nothing
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i = 0;
+
+	while(argv)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
