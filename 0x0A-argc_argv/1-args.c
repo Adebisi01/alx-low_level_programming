52f87ddab55a00e0659fec6d991bef7ae4ddf5f2
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-30 22:13:14 +0100

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - prints its own name
+ * @argv: argv
+ * @argc: argc
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
