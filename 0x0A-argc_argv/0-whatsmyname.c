cbc5e5241121217ec926b6b7f2c412f2e3f996b2
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-30 22:03:24 +0100

    argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
  * whatsmyname - prints its own name
+ * @argv: argv
+ * @argc: argc
  * Return: nothing
  */
-void main(argc, argv[0])
+void main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 }
