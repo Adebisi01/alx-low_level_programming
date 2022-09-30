f47e989a81e90d17d58e36d84cbd3db4f793854e
Oluwafemi <oluwafemiadebisi01@gmail.com>
2022-09-30 22:36:55 +0100

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - prints its own name
+ * @argv: argv
+ * @argc: argc
+ * Return: nothing
+ */
+int main(int argc, char *argv[])
+{
+	if (argc = 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argv <= 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
+	return (0);
+}
