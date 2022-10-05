fb3218c0ac92d8d3c10af601bbdab944f891a7c6
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-05 02:31:01 +0100

    malloc init

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,6 +16,10 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	str = malloc(size);
+	if (str == NULL)
+	{
+		printf("Cannot allocate %d bytes", size);
+	}
 	while (i < size)
 	{
 		str[i] = c;
