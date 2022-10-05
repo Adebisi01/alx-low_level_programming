7076deca60d0abf11c77bb13dc8547951faa33fd
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-05 04:22:14 +0100

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,7 +19,7 @@ int **alloc_grid(int width, int height)
 	if (space == NULL)
 		return (NULL);
 
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		space[i] =malloc(sizeof(int) * width);
 
