b557a55eb1dfa998336497b26487607cdb59618b
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-05 02:20:39 +0100

    malloc init

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,9 +3,20 @@
 /**
  * create_array - Create an array with malloc
  * @size: size
- * @char: character
+ * @c: character
+ * Return: Null or pointer
  */
 char *create_array(unsigned int size, char c)
 {
+	char *str;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	str = malloc(size);
+	str[0] = c;
+	return (str);
 }
 
