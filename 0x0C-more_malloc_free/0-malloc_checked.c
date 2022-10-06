2a69497ad81e9945677b7afe0f3f6d43ea94afe4
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-06 21:35:16 +0100

    allocation

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory 
+ * @b: integer to allocate memory 
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *ptr;
+
+	ptr = malloc(sizeof(b) * strlen(b));
+	if (ptr == NULL)
+		exit(98);
+}
