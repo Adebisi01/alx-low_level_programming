be5f1e89fc6215991ac3e4789a784bc180b1fdd1
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-06 22:27:54 +0100

    concatn

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	string = malloc(strlen(s1) + strlen(s2));
+	string = malloc(strlen(s1) + n);
 	if (string == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
