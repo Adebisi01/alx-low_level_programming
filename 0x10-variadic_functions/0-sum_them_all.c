5eb2023ddb4e0feaad819952387d7e3a5ad9c4a4
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-14 02:51:15 +0100

    sumn

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,11 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int i = 0,
+	unsigned int i = 0;
 	unsigned int sum = 0;
 
 	va_start(list, n);
-	for (i = 0; i < n, i++)
+	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(list, int);
 	}
