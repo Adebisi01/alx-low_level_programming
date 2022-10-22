506c725a6227a127f51f4378dfaa92a287cc3aab
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-22 22:30:29 +0100

    add

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * add_node - add node at the beginning of linked list
+ * @head: head of linked list
+ * @str: string of linked list
+ * Return: pointer to first node;
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	new->next = *head;
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	(*head) = new;
+	return (*head);
+}
+
