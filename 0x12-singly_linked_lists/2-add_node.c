23a6110cb7f88f8aa7e78604aeed88e57dfecd98
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-22 22:35:42 +0100

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,7 +12,7 @@ list_t *add_node(list_t **head, const char *str)
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-		return NULL;
+		return (NULL);
 	new->next = *head;
 	new->str = strdup(str);
 	new->len = strlen(new->str);
