7667b64b3c8d50d64d009c278fba54b60fdd6b69
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-24 22:06:00 +0100

    free

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint -  free node space
+ * @head: beggining
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while (head != NULL)
+	{
+		temp = head;
+
+		free(temp->n);
+		free(temp);
+		head = head->next;
+	}
+}
