3959e3adc8200475f0069ee46715f2083b8a302a
Adebisi <oluwafemiadebisi01@gmail.com>
2022-10-25 04:32:28 +0100

    get node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,4 +1,25 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index - delete and get node
- *
+ * get_nodeint_at_index - get nth node
+ * @head: beginning
+ * @index: nth
+ * Return: node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *temp;
+	unsigned int i = 0;
+
+	while (head != NULL)
+
+		if (i == index)
+		{
+			if (head == NULL)
+				return (NULL);
+			temp = head;
+		}
+		i++;
+		head = head->next;
+	}
+	return (temp);
+}
